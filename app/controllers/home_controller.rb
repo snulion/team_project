7174b2ac1ee568c905112786bfe874a7434d97e3
ParkSeungHwan <rr5ys5s@gmail.com>
2016-06-09 00:42:24 +0900

    크롤링만드는중

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,7 @@
+require 'nokogiri'
+require 'open-uri'
+
+
 class HomeController < ApplicationController
   def index
     if user_signed_in?
@@ -5,31 +9,32 @@ class HomeController < ApplicationController
     else
       redirect_to "/users/sign_in"
     end
+
   end
-  
+
   def post
     post_genre = params[:genre]
     post_content = params[:content]
     post_file = params[:file]
-    
+
     new_post = Post.new(genre: post_genre, content: post_content, file_name: post_file, user_id: current_user.id )
     new_post.save
-    
+
     redirect_to :back
   end
-  
+
   def like
     like_user = params[:like_user]
     post_id = params[:post_id]
     likeit = Like.new(user_id: like_user, post_id: post_id)
     likeit.save
-    
+
     redirect_to :back
   end
-  
+
   def recorder
   end
-  
+
   def show        # 각각의 POST 상세 보기
     if user_signed_in?
       post_id = params[:id]
@@ -38,15 +43,22 @@ class HomeController < ApplicationController
       redirect_to "/users/sign_in"
     end
   end
-  
+
   def comment     # Comment 기능 구현
     title = params[:title]
     content = params[:content]
     postid = params[:postid]
-    
+
+
     reply = Comment.new(title: title, content: content, post_id: postid, writer: current_user.email)
     reply.save
-    
+
+    ko = URI::encode("#{title}")
+    page = Nokogiri::HTML(open('http://search.bugs.co.kr/track?q='+ ko).read , nil, 'utf-8')
+    @orange = page.css('a.trackInfo')[0]['href']
+
+
+
     redirect_to :back
   end
 end
